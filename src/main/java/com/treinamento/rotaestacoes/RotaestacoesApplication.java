71e9f745a574767c0227ed1313a5d972bcf615e0
Mayan Barros da Silva <mayanbarrosdasilva@gmail.com>
2022-03-22 13:16:40 -0300

    Fist commit!

---
+++ src/main/java/com/treinamento/rotaestacoes/RotaestacoesApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.rotaestacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaestacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaestacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinamento/rotaestacoes/RotaestacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.rotaestacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaestacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
